617568c7c74d11b3fd7c017437209d98f9700d80
estebanquintana6 <a01184855@itesm.mx>
2018-11-13 10:43:17 -0600

    finished

--- neural_networks.py
+++ neural_networks.py
@@ -1,14 +1,38 @@
-import fileinput
-import math
 import random
+
+
+def activation_function(line, weights, threshold):
+    sum = 0
+    for i in range(0, len(line)):
+        sum += line[i] * weights[i]
+    if sum > threshold:
+        return 1
+    else:
+        return 0
+
+
+def calculate_error(train_data, weights, threshold):
+    o = train_data[1]
+    axon = activation_function(train_data[0], weights, threshold)
+    error = o - axon
+
+    return error
+
+
+def calculate_weights(train_data, weights, error):
+    for i, val in enumerate(weights):
+        weights[i] += error * train_data[0][i]
+
+    return weights
+
 
 if __name__ == "__main__":
     training_set = []
     test_data = []
     weights = []
 
-    iterations = 200
-    threshold = random.randrange(1,10)
+    iterations = 100
+    threshold = random.randrange(1, 10)
 
     dimensionality = int(input())
     training_set_length = int(input())
@@ -38,7 +62,20 @@ if __name__ == "__main__":
         rand = random.randrange(0, 1)
         weights.append(rand)
 
-    error = 0
     for i in range(iterations):
         # calculate error
-        print('error aqui')
+        error = 0
+
+        for t in training_set:
+            error_aux = calculate_error(t, weights, threshold)
+            weights = calculate_weights(t, weights, error_aux)
+            error += pow(error_aux, 2)
+
+        if error == 0:
+            break
+
+    if (error >= 1):
+        print("no solution found")
+    else:
+        for t in test_data:
+            print(activation_function(t, weights, threshold))
